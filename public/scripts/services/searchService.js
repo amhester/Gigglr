868ef6c3c5a43b56a7f6621a94f9a2c9f2944d9c
amhester <amhester@anderson.edu>
2015-11-17 09:06:35 -0500

    added timeouts to ajax calls

--- public/scripts/services/searchService.js
+++ public/scripts/services/searchService.js
@@ -7,6 +7,25 @@ class SearchService {
         $.ajax({
             method: 'GET',
             url: 'http://localhost:8179/search/' + q,
+            timeout: 7000,
+            success: function (res) {
+                console.log(JSON.stringify(res));
+                var data = res.map(function (o) { return new FunnyPost(o); });
+                callback(null, data);
+            },
+            error: function (err) {
+                callback(err);
+            }
+        });
+    }
+
+    static getFavorites (q, callback) {
+        var fUrl = 'http://localhost:8179/favorites/' + $.cookie('userContext');
+        fUrl += (q) ? '?q=' + q : '';
+        $.ajax({
+            method: 'GET',
+            url: fUrl,
+            timeout: 7000,
             success: function (res) {
                 console.log(JSON.stringify(res));
                 var data = res.map(function (o) { return new FunnyPost(o); });
